17911b2ed3a5ee416ba4c0f95fc871a6b7747259
sivakumar_b <sivakumar_b@solartis.com>
2025-08-17 20:20:18 +0530

    BrowserRouter changed into HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 import './index.css';
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
